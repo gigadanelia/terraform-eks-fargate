f2f9d11cd26b74c6c5c237e0d43c5129094ff671
Giga <giga.danelia@hotmail.com>
2025-02-26 23:34:45 +0400

    add terraform destroy

--- env/outputs.tf
+++ env/outputs.tf
@@ -1,10 +1,10 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
 
 # output "eks_cluster_name" {
 #   value = module.eks.cluster_name
