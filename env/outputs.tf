cd885329503be9c5431db5970e4dde92be5b3078
Giga <giga.danelia@hotmail.com>
2025-02-26 23:12:08 +0400

    add terraform destroy workflow

--- env/outputs.tf
+++ env/outputs.tf
@@ -6,10 +6,10 @@ output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "eks_cluster_name" {
+#   value = module.eks.cluster_name
+# }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "eks_cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
